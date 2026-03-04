fd7ae8cfdd3e8d848c87cc3f6cb06de98249b489
OxCSEC <comengcetinkaya@gmail.com>
2026-03-04 10:54:36 +0300

    start point

---
+++ src/main/java/com/example/appsec_pipeline/AppsecPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.appsec_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsecPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsecPipelineApplication.class, args);
+	}
+
+}
